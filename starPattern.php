9cceca3e0b5ba9ae78f12386d991c776117c6630
NjauSamuel <njaus602@gmail.com>
2024-11-26 23:11:51 +0300

    Did a remove duplicates chalenge.

--- starPattern.php
+++ starPattern.php
@@ -10,11 +10,23 @@ function web2()
     $n = 5;
 
      // Upper part of the diamond
-     for ($i = 0; $i < $n; $i++) {
-        // // Print spaces
-        // for ($j = 0; $j < $n - $i - 1; $j++) {
-        //     echo " ";
-        // }
+     for ($i = 1; $i < $n; $i++) {
+        // Print spaces
+        for ($j = 0; $j < $n - $i; $j++) {
+            echo " ";
+        }
+        // Print stars
+        for ($j = 0; $j < (2 * $i - 1); $j++) {
+            echo "*";
+        }
+        echo "\n"; // Move to the next line
+    }
+     // Lower part of the diamond
+    for ($i = $n - 1; $i >= 0; $i--) {
+        // Print spaces
+        for ($j = 0; $j < $n - $i - 1; $j++) {
+            echo " ";
+        }
         // Print stars
         for ($j = 0; $j < (2 * $i + 1); $j++) {
             echo "*";
